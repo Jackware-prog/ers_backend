a1023c024ef17bcf5773186c08896c1413ab21ca
SER YI JIET <seryijiet@gmail.com>
2024-12-13 21:24:39 +0800

    firstpush

---
+++ src/main/java/com/erwebsocket/ErwebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.erwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErwebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErwebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erwebsocket/ErwebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErwebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
